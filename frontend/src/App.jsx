d51edded3d483b5135ff177386bb40afc74670d2
SaimRaza885 <saimrazaytt@gmail.com>
2025-08-31 16:35:39 +0500

    dep4

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,7 +19,7 @@ const CreateProduct = lazy(() => import("./pages/Product/Create_product"));
 const AdminDashboard = lazy(() => import("./pages/dasboard/Admin"));
 const ProductImagesUpdate = lazy(() => import("./pages/Product/Update_product_images"));
 const Create_order = lazy(() => import("./pages/order/Order_Create"));
-const Cart = lazy(() => import("./pages/Cart/Cart.jsx"));
+const Cart = lazy(() => import("./pages/Cart/Cart"));
 const Dashboard = lazy(() => import("./pages/dasboard/User"));
 const SearchPage = lazy(() => import("./pages/Search_Page"));
 const AllOrders = lazy(() => import("./pages/order/All_Orders"));
